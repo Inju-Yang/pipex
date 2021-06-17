483837a79748346fd7e362694fcd611d679c565d
inyang <inyang@student.42seoul.kr>
2021-06-17 19:33:48 +0900

    start

---
+++ Makefile
@@ -0,0 +1,35 @@
+
+NAME			=	pipex
+CC				=	gcc
+CFLAGS_42		=	-Wall -Wextra -Werror
+AR				=	ar
+ARFLAGS			=	rcs
+RM				=	rm -rf
+
+PIPEX_F			=	./pipex.c\
+
+PIPEX_O			=	./pipex.o\
+
+LIBFT_D			=	./assets/libft/
+LIBFT_A			=	./assets/libft/libft.a
+
+.PHONY:	all clean fclean re bonus
+
+all: $(NAME)
+
+bonus: $(CHECK_NAME)
+
+$(NAME): $(PIPEX_O)
+	$(CC) $(CFLAGS_42) $(PIPEX_F)
+	mv a.out $(NAME)
+
+.c.o:
+	$(CC) $(CFLAGS_42) -c -o $@ $<
+
+clean:
+	$(RM) $(PIPEX_O) $(NAME)
+
+fclean: clean
+	make fclean
+
+re:	fclean all
---
+++ pipex.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inyang <inyang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/17 16:30:32 by inyang            #+#    #+#             */
+/*   Updated: 2021/06/17 16:45:23 by inyang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	redirect_in(const char *file)
+{
+	int fd;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+	{
+		perror(file);
+		return (-1);
+	}
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	return (0);
+}
+
+int		main(int ac, char **av, char **envp)
+{
+	redirect_in(infile);
+	return (0);
+}
