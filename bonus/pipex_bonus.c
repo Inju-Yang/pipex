c5efbdd859d2bf69e20b4f3728c7949f6a24347b
inyang <inyang@student.42seoul.kr>
2021-06-24 18:41:58 +0900

    jeong-ri-bon

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: inyang <inyang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 16:30:32 by inyang            #+#    #+#             */
-/*   Updated: 2021/06/24 18:20:38 by inyang           ###   ########.fr       */
+/*   Updated: 2021/06/24 18:41:32 by inyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,42 +33,28 @@ int	multi_pipe(int ac, char **av)
 		wait(NULL);
 		if (pipe(pipefd2) == -1)
 			printf("error here\n");
-
 		pid = fork();
-		printf("new fork here\n");
 		if (pid > 0)
 		{
-			printf("cmd 3 part\n");
 			wait(NULL);
-			printf("cmd 3 middle\n");
 			out_to_stdout(av[5]);
 			dup2(pipefd2[0], 0);
 			close(pipefd2[1]);
-//			close_pipe(pipefd2);
 			run_cmd(av[4]);
 		}
 		else if (pid == 0)
 		{
-			printf("cmd 2 part\n");
-			printf("pipefd[0] : %d, pipefd[1] : %d\n", pipefd[0], pipefd[1]);
 			dup2(pipefd[0], 0);
-//			close_pipe(pipefd);
 			dup2(pipefd2[1], 1);
 			close(pipefd2[0]);
-//			close_pipe(pipefd2[1]);
-			printf("cmd 2 part end\n");
 			run_cmd(av[3]);
 		}
 	}
 	else if (pid == 0)
 	{
-		printf("cmd 1 part\n");
-		printf("pipefd[0] : %d, pipefd[1] : %d\n", pipefd[0], pipefd[1]);
 		in_to_stdin(av[1]);
 		dup2(pipefd[1], 1);
 		close(pipefd[0]);
-//		close_pipe(pipefd);
-		printf("cmd 1 part end\n");
 		run_cmd(av[2]);
 	}
 	return (0);
@@ -106,15 +92,15 @@ int	main(int ac, char **av)
 	}
 	else if (pid == 0)
 	{
-		if (ac > 5)
+		if (ac > 6)
 			multi_pipe(ac, av);
 		else if (px_strcmp(av[1], "here_doc") == 0)
 		{
 			here_doc(ac, av);
 			here_doc_cmd(av);
 		}
-		else
-			multi_pipe(ac, av);
+		// else
+		// 	multi_pipe(ac, av);
 	}
 	return (0);
 }
